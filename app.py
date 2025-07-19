3e7cff2f77074c0a9dbda63a1a6c14f3d83904ee
MarianaBraga13 <maribragad@gmail.com>
2025-07-19 00:11:34 -0300

    Finalizando com feature balloons

--- app.py
+++ app.py
@@ -6,9 +6,17 @@ df = pd.read_csv("pecas.csv")
 
 modelo = LinearRegression()
 x = df[["diametro"]]
-y = df["preco"]
+y = df[["preco"]]
+
+modelo.fit(x,y)
 
 st.title("Previsão de valores no mercado")
 st.title("Peças do tipo A12Bx-260")
+st.divider()
 
+diametro = st.number_input("Digite o diâmetro da peça: ")
+if diametro:
+    preco_previsto = modelo.predict([[diametro]])[0][0]
+    st.write(f'Conforme pesquisas e dados recebidos, a tendência de valor de mercado para essa peça com diâmetro de {diametro:.2f}cm atualmente é de: R$ {preco_previsto:.2f} /un.')
+    st.balloons()
 
